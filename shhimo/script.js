5410ad3decea0927bbf387d8b294bbf1a6d14728
Chris Thomas <webmaster102@gmail.com>
2022-09-17 12:30:15 -0400

    /shhimo app splash decreased timeout

--- shhimo/script.js
+++ shhimo/script.js
@@ -2,7 +2,7 @@ const splash = document.querySelector(".splash");
 document.addEventListener('DOMContentLoaded', (e) => {
   setTimeout(() => {
     splash.classList.add('display-none');
-  }, 2000);
+  }, 1000);
 })
 
 //initialize variables
